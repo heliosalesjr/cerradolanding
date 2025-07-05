22b0211a9cf1db6e6d2bf60e242eda5b71a28f53
Helio Sales Jr <heliosalesjr@gmail.com>
2025-07-04 21:31:15 -0400

    extra component content

--- components/Extra.jsx
+++ components/Extra.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Extra = () => {
   return (
-    <div>Extra</div>
+    <div>
+        <h1>
+            Extra, extra!
+        </h1>
+
+        <p>lorem ipsum laureat</p>
+    </div>
   )
 }
 
